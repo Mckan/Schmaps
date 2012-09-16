b9c126fe447b00bde77c54ca5a78a7c960ff88e3
Frolle <frollix@razzan.net>
2012-09-16 14:27:45 +0200

    Added intent to start mapactivity when button is clicked

--- src/com/chalmers/schmaps/MenuActivity.java
+++ src/com/chalmers/schmaps/MenuActivity.java
@@ -2,7 +2,10 @@ package com.chalmers.schmaps;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
 
 public class MenuActivity extends Activity {
 
@@ -10,6 +13,14 @@ public class MenuActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+        Button searchHall = (Button) findViewById(R.id.button1);
+        searchHall.setOnClickListener(new View.OnClickListener() {
+			
+			public void onClick(View arg0) {
+				Intent startMapActivity = new Intent("android.intent.action.MAPACTIVITY");
+				startActivity(startMapActivity);
+			}
+		});
     }
 
     @Override
