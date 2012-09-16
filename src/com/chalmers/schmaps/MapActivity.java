55386880b52222bac336b4410d4a75c024be864a
Frolle <frollix@razzan.net>
2012-09-16 15:28:42 +0200

    Added some preliminary code for checking strings on edit text

--- src/com/chalmers/schmaps/MapActivity.java
+++ src/com/chalmers/schmaps/MapActivity.java
@@ -1,8 +1,14 @@
 package com.chalmers.schmaps;
 
+import java.util.HashMap;
+
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MapActivity extends Activity {
 
@@ -10,6 +16,27 @@ public class MapActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
+        final HashMap<String, Integer> lectureHashMap = new HashMap();
+        final Button editButton = (Button) findViewById(R.id.edittextbutton);
+        final EditText lectureEdit = (EditText) findViewById(R.id.edittextlecture);
+        lectureEdit.setOnTouchListener(new View.OnTouchListener() {
+			
+			public boolean onTouch(View arg0, MotionEvent arg1) {
+				lectureEdit.setText("");
+				return false;
+			}
+		});
+        editButton.setOnClickListener(new View.OnClickListener() {
+			
+			public void onClick(View arg0) {
+				String lectureText = lectureEdit.getText().toString();
+				if(lectureHashMap.containsKey(lectureText))
+						{
+							//TODO A method that returns coordinates to database.
+						}
+				
+			}
+		});
     }
 
     @Override
