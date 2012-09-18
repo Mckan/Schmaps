12ed7b097a747b026f07a63a5b32ea02371a6fed
Martin Augustsson <mckan91@gmail.com>
2012-09-18 12:25:59 +0200

    blaa

--- src/com/chalmers/schmaps/MenuActivity.java
+++ src/com/chalmers/schmaps/MenuActivity.java
@@ -3,6 +3,7 @@ package com.chalmers.schmaps;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.widget.ImageView;
 
 public class MenuActivity extends Activity {
 
@@ -10,6 +11,7 @@ public class MenuActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+
     }
 
     @Override
